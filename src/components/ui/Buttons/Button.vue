ebe73793523f778016b978fa92407117f4bca8aa
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2025-06-07 16:06:45 -0300

    style(button): change button style

--- src/components/ui/Buttons/Button.vue
+++ src/components/ui/Buttons/Button.vue
@@ -22,7 +22,7 @@ const props = defineProps({
 })
 
 const computedClass = computed(() => {
-    const baseClass = 'font-["Nunito"] text-white px-4 py-2 rounded cursor-pointer transition-colors duration-300';
+    const baseClass = 'font-["Nunito"] text-white text-sm md:text-base px-4 py-2 rounded cursor-pointer transition-colors duration-300';
     const typeClass = {
         primary: 'bg-[#355C7D] hover:bg-[#2C4B66] active:bg-[#22384F]',
         secondary: 'bg-[#5C6B7A] hover:bg-[#4A5864] active:bg-[#3A4752]',
