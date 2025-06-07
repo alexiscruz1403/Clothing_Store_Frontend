16afdf447ae27695a0cdaf9ee2dbf8db92048f2e
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2025-06-07 16:04:21 -0300

    refactor: refactored GenreCard, CategoryCard and HomeView
    refactored the view and components to use the implementation of stores

--- src/components/ui/Cards/CategoryCard.vue
+++ src/components/ui/Cards/CategoryCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[#A89272] hover:bg-[#7E6E56] active:bg-[#3A3227] h-52 md:h-60 lg:h-72 min-w-40 md:w-1/4 cursor-pointer text-[#2E2E2E] font-[DMSans] relative rounded-lg transition-colors duration-300 ease-in-out p-4">
+    <div class="bg-[#A89272] hover:bg-[#7E6E56] active:bg-[#3A3227] h-52 md:h-60 lg:h-72 min-w-40 md:w-1/4 cursor-pointer text-[#2E2E2E] font-[DMSans] relative rounded-lg transition-colors duration-300 ease-in-out p-4" @click="handleCardClick">
         <img :src="props.categoryImage" alt="" class="h-5/6 w-full object-cover rounded-t-lg">
         <div class="py-3 px-2 absolute bottom-0 left-0 w-full">
             <p class="text-lg md:text-base lg:text-xl text-center">{{ props.categoryName }}</p>
@@ -7,6 +7,9 @@
     </div>
 </template>
 <script setup>
+
+import { useFiltersStore } from '@/stores/useFiltersStore';
+import router from '@/router';
 
 const props = defineProps({
     categoryId: {
@@ -22,5 +25,12 @@ const props = defineProps({
         required: true
     }
 })
+
+const filtersStore = useFiltersStore();
+
+const handleCardClick = () => {
+    filtersStore.setCategory(props.categoryId);
+    router.push({ name: 'products' });
+}
 
 </script>
