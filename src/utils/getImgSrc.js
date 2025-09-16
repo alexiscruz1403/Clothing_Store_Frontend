b122170be1cff28f92211a17ebc79ad4533a15bf
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2025-09-15 22:51:54 -0300

    fix: fix get-img-src util url

--- src/utils/getImgSrc.js
+++ src/utils/getImgSrc.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://clothingstorebackend-production-8f6e.up.railway.app';
 
 export const getImgSrc = (imageUrl) => {
     return imageUrl ? `${BASE_URL}${imageUrl}` : `${BASE_URL}/storage/default.png`;
